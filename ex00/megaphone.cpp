7aa3acf4a1ec8092b3403ee9c38eebae9734688c
Yixin Zhang <yizhang@f0r5s6.codam.nl>
2023-09-21 16:49:38 +0200

    next trans lowercase to uppercase dont know how

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,33 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/21 15:12:26 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/09/21 15:53:42 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/09/21 16:46:06 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout<<"hello \n";
+	if (argc <= 1)
+	{
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			int	len = argv[i].length();
+			for (int j = 0; j < len; j++)
+			{
+				if (islower(argv[i][j]))
+					argv[i][j] = toupper(argv[i][j]);
+			}
+			std::cout<<argv[i]<<" ";
+		}
+		std::cout<<"\n";
+	}
+	return (0);
 }
