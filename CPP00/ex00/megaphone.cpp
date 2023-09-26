9e6d7cdd29a5e423d8e5e4b62d4e17c6b648a130
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-09-26 10:16:47 +0200

    ok

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/09/21 15:12:26 by yizhang       #+#    #+#                 */
+/*   Updated: 2023/09/26 09:24:53 by yizhang       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	
+	i = 1;
+	j = 0;
+	if (argc <= 1)
+	{
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		return (1);
+	}
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				std::cout<<(char)(std::toupper(argv[i][j]));
+				j++;
+			}
+			if (i != argc - 1)
+				std::cout<<" ";
+			i++;
+		}
+		std::cout<<std::endl;
+	}
+	return (0);
+}
