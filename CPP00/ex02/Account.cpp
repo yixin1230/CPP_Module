147009224e8e48b34818fcad975593573f9a7db1
Yixin Zhang <yizhang@f1r1s8.codam.nl>
2023-10-03 18:16:23 +0200

    start ex02

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,76 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/26 10:41:24 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/09/26 10:41:25 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/10/03 18:07:30 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
+
+//why I move the 'static'?
+//As long as you have the static keyword in the header file,
+//the compiler knows it's a static class method,
+//so you should not and cannot specify it in the definition in the source file.
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+		std::cout<<"index:"<<i<<';';
+		std::cout<<"amount:"<<getTotalAmount()<<';';
+		std::cout<<"deposits:"<<getNbDeposits()<<';';
+		std::cout<<"withdrawals:"<<getNbWithdrawals()<<';';
+}
+
+Account::Account( int initial_deposit )
+{
+	
+}
+
+Account::~Account( void )
+{
+	
+}
+
+void	makeDeposit( int deposit )
+{
+	
+}
+
+bool	makeWithdrawal( int withdrawal )
+{
+	if ()
+	{
+		
+	}
+	else
+		return ();
+}
+
+int		checkAmount( void ) const
+{
+	return ();
+}
+
+void	displayStatus( void ) const
+{
+	
+}
