fcdd2d68be847a67e7b18b2225a3b754c82d2216
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-11-17 15:38:01 +0100

    start ex03

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/17 10:49:39 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/11/17 14:42:43 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/11/17 14:58:51 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 int main()
 {
 	std::string brain;
-	std::string *stringPTR;
-	std::string &stringREF;
+	std::string *stringPTR = &brain;
+	std::string &stringREF = brain;
 
 	brain = "HI THIS IS BRAIN";
-	stringPTR = &brain;
-	stringREF = brain;
 
-	std::cout<<&brain<<std::endl;
+	std::cout<<"address of string: "<<&brain<<std::endl;
+	std::cout<<"address of PTR:    "<<&stringPTR<<std::endl;
+	std::cout<<"address of REF:    "<<&stringREF<<std::endl;
+
+	std::cout<<"value of string: "<<brain<<std::endl;
+	std::cout<<"value of PTR:    "<<stringPTR<<std::endl;
+	std::cout<<"value of REF:    "<<stringREF<<std::endl;
+	
 	return 0;
 }
