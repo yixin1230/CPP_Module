f077b09fda01a624d46720e9fc83aa0ef73d6e17
YIXIN ZHANG <yixinzhang@YIXINs-Air.home>
2023-12-20 23:01:32 +0100

    ok

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yixinzhang <yixinzhang@student.codam.nl      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/20 15:57:23 by yixinzhang    #+#    #+#                 */
-/*   Updated: 2023/12/20 17:05:13 by yixinzhang    ########   odam.nl         */
+/*   Updated: 2023/12/20 22:51:40 by yixinzhang    ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,47 @@
 #include <fstream> 
 #include <iostream>
 
-int	main()
+
+int	main(int argc, char **argv)
 {
 	//open a file 
 	//read all file,
 	//and copy all the content to new file
 	//if in side the new file have s1, replace it to s2
-	std::fstream file;
-	std::string s1;
-	std::string s2;
-	
-	
-	file.open("Makefile",std::ios::in);
 	//out mode for write only
 	//app for append
 	//in for read mode
 	
+	std::fstream file;
+	std::fstream re_file;
+	std::string replace;
+	std::string file_name;
+	std::string s1;
+	std::string s2;
+	
+	if (argc != 4)
+	{
+		std::cout<<"this program should have three parameters"<<std::endl;
+		return 1;
+	}
+	replace = ".replace";
+	file_name = argv[1] + replace;
+	file.open(argv[1],std::ios::in);
 	if (file.is_open()){
-		std::cout<<"file is open"<<std::endl;
-		file<<"I wrote something"<<std::endl;
+		re_file.open(file_name,std::ios::app);
+		while (getline(file,s1))
+		{
+			re_file<<s1<<std::endl;
+		}
 	}
 	else
 	{
-		std::cout<<"dobn't have file"<<std::endl;
+		std::cout<<"don't have file"<<std::endl;
+		re_file.close();
+		file.close();
+		return 2;
 	}
-	//name of the file to be open, which mode
-	//out is the write mode.
+	re_file.close();
 	file.close();
 	return 0;
 }
