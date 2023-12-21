69b98f7a0f85b49d507303096576138fa359b1da
Yixin <zhaozicen951230@gmail.com>
2023-12-22 00:30:13 +0100

    try to understant what funciton pointer is

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,31 +6,45 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/21 20:38:45 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/12/21 20:40:17 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/12/22 00:25:34 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+Harl::Harl()
+{
+	std::cout<<"Harl is born"<<std::endl;
+}
+
+Harl::~Harl()
+{
+	std::cout<<"Harl is gone"<<std::endl;
+}
+
 void	Harl::debug(void)
 {
-		
+	std::cout<<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"<<std::endl;
 }
 
 void	Harl::info(void)
 {
-	
-
+	std::cout<<"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"<<std::endl;
 }
 
 void	Harl::warning(void)
 {
-	
-
+	std::cout<<"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."<<std::endl;
 }
 
 void	Harl::error(void)
 {
-	
+	std::cout<<"This is unacceptable! I want to speak to the manager now."<<std::endl;
 
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string func[] = {"debug", "info", "warning", "error"};
+	void(*funcP)() = Harl::debug(); 
 }
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,11 +6,14 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/21 20:03:07 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/12/21 20:10:59 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/12/22 00:14:57 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+//function pointer:
+//be able to pass a function as an argument or as a parameter to another function.
 
 int	main()
 {
