8553cfedbb51e90d8ed0550e8bd303291473f4d5
Yixin <zhaozicen951230@gmail.com>
2023-12-22 17:38:10 +0100

    done

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -6,13 +6,13 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/17 15:03:22 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/11/17 18:06:26 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/12/22 17:34:12 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 #include "HumanB.hpp"
-//#include <stdlib.h>
+
 int main()
 {
 	{
@@ -30,6 +30,6 @@ int main()
 		club.setType("some other type of club");
 		jim.attack();
 	}
-	//system("leaks");
+
 	return 0;
 }
