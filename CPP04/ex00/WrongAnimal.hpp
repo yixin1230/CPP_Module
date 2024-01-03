53a1c44358dd0c3432121e1dda283fcfcff4e7c9
Yixin <zhaozicen951230@gmail.com>
2024-01-03 23:18:33 +0100

    add wrong animal and wrong cat in side

---
+++ CPP04/ex00/WrongAnimal.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   WrongAnimal.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/03 21:43:31 by yizhang       #+#    #+#                 */
+/*   Updated: 2024/01/03 21:50:04 by yizhang       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+#define WRONGANIMAL_HPP
+
+class WrongAnimal
+{
+	
+};
+#endif
