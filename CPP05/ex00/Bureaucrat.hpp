8232ab76d5328ef9bf043d152711b9646cd9a171
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2024-01-18 13:20:44 +0100

    start cpp05

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/18 13:15:18 by yizhang       #+#    #+#                 */
+/*   Updated: 2024/01/18 13:20:11 by yizhang       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+    public:
+        g
+    private:
+    	std::string const	_name;
+    	int					_grade;
+};
+
+# endif
