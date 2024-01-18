6ae859e1827b17e3dee49721f471799211b99f6e
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2024-01-18 14:04:59 +0100

    start cpp05

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/18 13:26:29 by yizhang       #+#    #+#                 */
-/*   Updated: 2024/01/18 13:27:43 by yizhang       ########   odam.nl         */
+/*   Updated: 2024/01/18 14:04:39 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Bureaucrat::Bureaucrat()
 {
-    
+    std::cout<<"Bureaucrat constructor called"<<std::endl;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &a)
@@ -24,21 +24,23 @@ Bureaucrat::Bureaucrat(const Bureaucrat &a)
 
 Bureaucrat::~Bureaucrat()
 {
-    
+    std::cout<<"Bureaucrat destructor called"<<std::endl;
 }
 
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &a)
 {
-    
+    this->_grade = a._grade;
+    this->_name = a.getName();
+    return (*this);
 }
 
 std::string	Bureaucrat::getName(void)
 {
-    
+    return (_name);
 }
 
 int	Bureaucrat::getGrade(void)
 {
-    
+    return (_grade);
 }
