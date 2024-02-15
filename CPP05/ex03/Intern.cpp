3f1e9324022c7ce6fcbc1bd28221b193c7939850
Yixin <zhaozicen951230@gmail.com>
2024-02-15 22:21:25 +0100

    done ex03 but have error in it

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -6,8 +6,72 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/14 14:08:11 by yizhang       #+#    #+#                 */
-/*   Updated: 2024/02/14 14:08:26 by yizhang       ########   odam.nl         */
+/*   Updated: 2024/02/15 22:20:04 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+
+Intern::Intern()
+{
+    std::cout<<"Intern default constructor called"<<std::endl;
+}
+
+Intern::Intern(Intern const &src)
+{
+    std::cout<<"Intern copy constructor called"<<std::endl;
+}
+
+Intern::~Intern()
+{
+    std::cout<<"Intern destructor called"<<std::endl;
+}
+
+Intern &Intern::operator=(Intern const &src)
+{
+    (void)src;
+    return (*this);
+}
+
+Aform *Intern::makeForm(std::string name, std::string target)
+{
+    std::string FormName[3] ={
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"
+    };
+    
+    Aform *(Intern::*FunctionPointer[3])(std::string target) = {
+        Intern::makePresidentialPardonForm,
+        Intern::makeRobotomyRequestForm,
+        Intern::makeShrubberyCreationForm
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (target == FormName[i])
+        {
+            return ((FunctionPointer[i])(target));//what is wrong in here?
+        }
+    }
+    throw Intern::InternExeception();
+    
+}
+
+Aform *Intern::makePresidentialPardonForm(std::string target)
+{
+    PresidentialPardonForm *form = new PresidentialPardonForm(target);
+    return (form);
+}
+
+Aform *Intern::makeRobotomyRequestForm(std::string target)
+{
+    RobotomyRequestForm *form = new RobotomyRequestForm(target);
+    return (form);
+}
+
+Aform *Intern::makeShrubberyCreationForm(std::string target)
+{
+    ShrubberyCreationForm *form = new ShrubberyCreationForm(target);
+    return (form);
+}
