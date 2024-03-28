4047338eb368b2a39b156ddee45ddcfcaf6be92a
Yixin Zhang <yizhang@f0r6s4.codam.nl>
2024-03-28 14:41:54 +0100

    add

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,11 +6,34 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/27 21:33:41 by yizhang       #+#    #+#                 */
-/*   Updated: 2024/02/27 21:33:49 by yizhang       ########   odam.nl         */
+/*   Updated: 2024/03/28 12:48:24 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
 int main()
 {
-    
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
