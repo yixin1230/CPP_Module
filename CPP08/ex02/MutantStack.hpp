d2fa82a119ccd81c259eb627cffb2144a2cbef3f
Yixin <zhaozicen951230@gmail.com>
2024-03-29 15:34:38 +0100

    add readme

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   MutantStack.hpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/03/29 13:57:01 by yizhang       #+#    #+#                 */
+/*   Updated: 2024/03/29 15:33:41 by yizhang       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template <typename T, typename T2 = std::deque<T>>
+//deque: is an indexed sequence container that allows fast insertion and deletion at both its beginning and its end.
+
+class MutantStack: public std::stack<T>
+{
+    
+};
+
+#endif
