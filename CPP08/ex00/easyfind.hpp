831312dfc78374b1d943eb5e24fba684f29a1123
Yixin <zhaozicen951230@gmail.com>
2024-07-13 17:32:08 +0800

    ok

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/27 13:37:36 by yizhang       #+#    #+#                 */
-/*   Updated: 2024/02/28 19:26:17 by yizhang       ########   odam.nl         */
+/*   Updated: 2024/07/13 17:05:54 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <iostream>
 
 template<typename T>
+// T is a container of integers
+// this function has to find the first occurrnce of the second parameter
+// in the first parameter
 typename T::iterator easyfind(T &container, int value)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
